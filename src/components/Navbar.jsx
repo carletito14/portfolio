6a419ed49249b212a17e7f20688069b60c1b2378
carletito14 <contactomanuelgutierrez@gmail.com>
2022-06-29 08:52:43 +0200

    some links updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Navbar = () => {
-    
+
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
 
     return (
-        
+
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
             <div>
                 <img src={logo} alt="Logo" style={{ width: '50px' }} />
@@ -45,21 +45,22 @@ const Navbar = () => {
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                         <a className='flex justify-between items-center w-full text-gray-300'
-                            href='/'>
+                            href='https://www.linkedin.com/in/manuel-gutiérrez-campos/'
+                            target="_blank" rel="noreferrer">
                             Linkedin <FaLinkedin size={30} />
                         </a>
                     </li>
 
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                         <a className='flex justify-between items-center w-full text-gray-300'
-                            href='/'>
+                            href='https://github.com/carletito14' target="_blank" rel='noreferrer'>
                             GitHub <FaGithub size={30} />
                         </a>
                     </li>
 
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                         <a className='flex justify-between items-center w-full text-gray-300'
-                            href='/'>
+                            href='mailto:contactomanuelgutierez@gmail.com' target="_blank" rel='noreferrer'>
                             Email <HiOutlineMail size={30} />
                         </a>
                     </li>
