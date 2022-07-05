faaac114a931f28fbb6a8e8b292f83211c4d9b5b
carletito14 <contactomanuelgutierrez@gmail.com>
2022-07-05 12:00:02 +0200

    new changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
-import Dbs from '../assets/dbs.jpeg';
-import Everis from '../assets/everis.webp';
-import Telefonica from '../assets/telefonica.png';
+import { works } from '../data/data';
 
 const Work = () => {
-  return (
+  const data = works.map(function (work) {
+    return (
+      <div
+        style={{ backgroundImage: `url(${work.img})` }}
+        className='shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div'
+      >
+      </div>
+    );
+
+  }); return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
@@ -16,29 +23,8 @@ const Work = () => {
         </div>
 
         {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          {/* Grid Item 1*/}
-          <div
-            style={{ backgroundImage: `url(${Dbs})` }}
-            className='shadow-lg shadow-[#313131] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-          </div>
-
-          {/* Grid Item 1*/}
-          <div
-            style={{ backgroundImage: `url(${Everis})` }}
-            className='shadow-lg shadow-[#313131] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-          </div>
-
-
-          {/* Grid Item 1*/}
-          <div
-            style={{ backgroundImage: `url(${Telefonica})` }}
-            className='shadow-lg shadow-[#313131] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-          </div>
-
+        <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-2'>
+          {data}
         </div>
       </div>
     </div>
---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+import dbs from "../assets/dbs.jpeg"
+import everis from "../assets/everis.webp"
+import telefonica from "../assets/telefonica.png"
+import acciona from "../assets/acciona.png"
+
+export const works = [
+    { name: "DBS", img: dbs },
+    { name: "EVERIS", img: everis },
+    { name: "TELEFÓNICA", img: telefonica },
+    { name: "ACCIONA", img: acciona },
+
+]
