6ccf1bdb9bc3cb91fe992abc8fb808b632f9d1f2
carletito14 <contactomanuelgutierrez@gmail.com>
2022-07-05 13:11:57 +0200

    improving styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-yellow-200 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - contactomanuelgutierrez@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+            <input className='bg-[#ccd6f6] p-3 rounded-xl' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="5" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-yellow-500 hover:border-yellow-200 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
