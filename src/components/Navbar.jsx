b6972c5b111444af8967ee6ed0610e4db1fc911d
carletito14 <contactomanuelgutierrez@gmail.com>
2022-07-05 12:40:08 +0200

    new styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav);
 
     return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#023047] text-gray-300'>
             <div>
                 <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
             </div>
@@ -60,7 +60,7 @@ const Navbar = () => {
                 className={
                     !nav
                         ? 'hidden'
-                        : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+                        : 'absolute top-0 left-0 w-full h-screen bg-[#023047] flex flex-col justify-center items-center'
                 }
             >
                 <li className='py-6 text-4xl'>
